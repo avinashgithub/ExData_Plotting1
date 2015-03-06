22fc0803cfb590df320724a5ba848bbfbbb9f8a1
AA <avinash.agrawal@j2xl.com>
2015-03-06 07:08:26 -0800

    Initial commit.

---
+++ plot4.R
@@ -0,0 +1,54 @@
+# Given the number of rows in the table is too large, let us divide it into
+# two, process first half, clear the raw data from memory, and then
+# repeat for the seond half. Had to do this due to memory shortage on my device.
+
+power <- read.table(
+    './exdata-data-household_power_consumption//household_power_consumption.txt', 
+    header=T, sep=';', na.strings="?", nrow = 1000000)
+power[,1] <- as.Date(power[,1], '%d/%m/%Y')
+
+# Keep only the data we are interested in, and remove the rest
+febSet <-  subset(power, power$Date == '2007-02-01'| power$Date == '2007-02-02')
+rm(power)
+
+# Now do the same for the second half of the file
+power2 <- read.table(
+    './exdata-data-household_power_consumption//household_power_consumption.txt', 
+    header=T, sep=';', na.strings="?", skip = 1000000)
+power2[,1] <- as.Date(power2[,1], '%d/%m/%Y')
+
+febSet2 <- subset(power2, power2$Date == '2007-02-01'| power2$Date == '2007-02-02')
+rm(power2)
+febSet <- rbind(febSet, febSet2)
+rm(febSet2)
+
+# Initialize appropriately
+datetime<- as.POSIXct(paste(febSet$Date, febSet$Time))
+febSet <- cbind(febSet, datetime)
+
+# Plot 4
+png('./ExData_Plotting1/plot4.png', width = 480, height=480, units='px')
+par(mfrow=c(2,2))
+# Graph in 1,1 position
+datetime<- as.POSIXct(paste(febSet$Date, febSet$Time))
+febSet <- cbind(febSet, datetime)
+plot(febSet$datetime, febSet$'Global_active_power', 
+     ylab='Global Active Power',
+     type='l', xlab='')
+
+# Graph in 1, 2 position
+with(febSet, plot(datetime, Voltage, type='l'))
+
+# Graph in 2, 1 position
+with (febSet, {
+    plot(datetime, Sub_metering_1, ylab = "Energy sub metering", xlab="", type='l')
+    points(datetime, Sub_metering_2, type='l', col='red')
+    points(datetime, Sub_metering_3, type='l', col='blue')
+})
+legend("topright", lwd = 1, col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       bty='n')
+
+#Graph in 2, 2 position
+with(febSet, plot(datetime, Global_reactive_power, type='l'))
+dev.off()
